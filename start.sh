b7b22b30932fab09b80da5d10aa8770b3acfbc87
Adam Presley <adam@adampresley.com>
2013-01-17 06:55:09 -0600

    Linux start script now correctly sets CATALINA_HOME

--- start.sh
+++ start.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-
-#
-# Start MongoDB
-#
-#server/mongodb/mongod --fork -f server/mongodb/config
-
-
 #
 # Start Tomcat
 #
+export CATALINA_HOME="./server/tomcat"
 server/tomcat/bin/catalina.sh run
-
-#
-# Stop MongoDB
-#
-#kill -2 `pidof mongod`
